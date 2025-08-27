834781fda1c137e053936f43cace46a93966c44b
JMartiniano <jose.martiniano01@gmail.com>
2025-08-26 21:17:21 -0300

    Subindo VPC e Subents

--- vpc-projeto-eks.tf
+++ vpc-projeto-eks.tf
@@ -1,6 +1,8 @@
 resource "aws_vpc" "vpc_projeto_eks" {
-  cidr_block       = "10.10.0.0/16"
-  instance_tenancy = "default"
+  cidr_block           = "10.10.0.0/16"
+  instance_tenancy     = "default" # Default as EC2 usam DC compartilhado e Dedicated usa DC dedicados
+  enable_dns_support   = true # default é true mas add por boa pratica
+  enable_dns_hostnames = true # tem que ativar
 
   tags = {
     Name      = "Vpc-Projeto-EKS"
